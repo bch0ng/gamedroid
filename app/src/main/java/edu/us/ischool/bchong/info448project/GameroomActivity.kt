6138b05269de7be21aee32fb712e1f2524fa477f
boycao <bycao96@uw.edu>
2019-05-23 00:22:04 -0700

    Initiate Game system(Game room, gamelist, scoreboard)

---
+++ app/src/main/java/edu/us/ischool/bchong/info448project/GameroomActivity.kt
@@ -0,0 +1,28 @@
+package edu.us.ischool.bchong.info448project
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+
+class GameroomActivity : AppCompatActivity(){
+
+    private lateinit var HostButton : Button
+    private lateinit var HostGuest1 : Button
+    private lateinit var HostGuest2 : Button
+    private lateinit var HostGuest3 : Button
+    private lateinit var HostGuest4 : Button
+    private lateinit var HostGuest5 : Button
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        
+    }
+
+
+
+
+
+
+
+}
