b8195a9e699118853904ff1cc56a0a1f90400c20
Yvonne61m <yvonne61m@gmail.com>
2019-06-01 11:36:43 -0700

    fixed UI

--- app/src/main/java/edu/us/ischool/bchong/info448project/SelectPlayer_Activity.kt
+++ app/src/main/java/edu/us/ischool/bchong/info448project/SelectPlayer_Activity.kt
@@ -3,11 +3,14 @@ package edu.us.ischool.bchong.info448project
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class SelectPlayer_Activity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_selectplayer)
 
+        val btnSingle = findViewById<Button>(R.id.btnSingle)
+        val btnMulti = findViewById<Button>(R.id.btnMulti)
     }
 }
