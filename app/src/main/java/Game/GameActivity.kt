e2911f4e0aae4781c7c218c3fda3bc9c25687338
boyacao <bycao96@uw.edu>
2019-06-10 11:29:46 -0700

    Empty the Gameroomactivity for connection merge, bugs still apear in gameactivity

--- app/src/main/java/Game/GameActivity.kt
+++ app/src/main/java/Game/GameActivity.kt
@@ -4,9 +4,10 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
+import android.support.v7.app.AppCompatActivity
 import edu.us.ischool.bchong.info448project.R
 
-class GameActivity : FragmentActivity(), GamelistFragment.OnGameInteractionListener, ScoreBoardFragment.OnScoreboardInteractionListener{
+class GameActivity : AppCompatActivity(), GamelistFragment.OnGameInteractionListener, ScoreBoardFragment.OnScoreboardInteractionListener{
     /*override fun onFragmentInteraction(uri: Uri) {
         //Something
 }*/
@@ -18,22 +19,23 @@ class GameActivity : FragmentActivity(), GamelistFragment.OnGameInteractionListe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        onGameSelect()
-    }
-
-    override fun onGameSelect() {
         identity = intent.getStringExtra("IDENTITY")
         mode = intent.getStringExtra("GAMEMODE")
+        onGameSelect(identity,mode)
+    }
+
+    override fun onGameSelect(playmode: String,useridentity: String) {
+
         /*game = intent.extras.getSerializable("GAME") as Game
         var gameFragment = game.gameFragment as Fragment*/
-        if(mode == "Single" && identity=="Host"){
+        if(playmode == "Single" && useridentity=="Host"){
             val gameSelectionFragment =
-                GamelistFragment.newInstance(mode, identity)
+                GamelistFragment.newInstance(playmode, useridentity)
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.game_frame, gameSelectionFragment!!, "game_fragment")
                 .commit()
-        }else if(mode == "Multi" && identity =="Host"){
+        }else if(playmode == "Multi" && useridentity =="Host"){
             //TODO
         }else{
             //TODO
@@ -52,8 +54,8 @@ class GameActivity : FragmentActivity(), GamelistFragment.OnGameInteractionListe
         game.OnStart()
     }
 
-    override fun onGameResult(){
-        val scoreBoardFragment = ScoreBoardFragment.newInstance()
+    override fun onGameResult(username:String,useridentity:String,gamechoice: String,userscore:String, playmode:String){
+        val scoreBoardFragment = ScoreBoardFragment.newInstance(username,useridentity, gamechoice, userscore, playmode)
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.game_frame, scoreBoardFragment!!, "game_fragment")
