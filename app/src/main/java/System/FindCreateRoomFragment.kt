9dee9cdabecbca03713cff5d3d297c37dc9c2383
bch0ng <bchong@uw.edu>
2019-06-10 16:33:23 -0700

    [Fix] Moves fragments to System package

--- app/src/main/java/System/FindCreateRoomFragment.kt
+++ app/src/main/java/System/FindCreateRoomFragment.kt
@@ -1,4 +1,4 @@
-package edu.us.ischool.bchong.info448project
+package System
 
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -17,6 +17,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import edu.us.ischool.bchong.info448project.NearbyConnection
+import edu.us.ischool.bchong.info448project.R
 
 /**
  * A simple [Fragment] subclass.
